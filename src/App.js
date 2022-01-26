0d350654361d2393e0ad97ebca4b562882b620eb
BlueHarry <harryhuang2012@gmail.com>
2022-01-26 20:49:49 +0800

    V0.1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
 import { Button } from '@mui/material';
+import styled from '@emotion/styled';
+
+const BlueGrid = styled.div`
+  width: 100%;
+  padding: 20px;
+  border: 5px red solid;
+  margin: auto;
+  display: flex;
+  justify-content: space-around;
+`;
 
 const App = () => {
   return (
-    <div>
-      <Button> mui-button </Button>
-    </div>
+    <BlueGrid>
+      <Button variant='outlined' size='large'>
+        {' '}
+        mui-button1{' '}
+      </Button>
+      <Button variant='outlined' size='large'>
+        {' '}
+        mui-button2{' '}
+      </Button>
+      <Button variant='outlined' size='large'>
+        {' '}
+        mui-button3{' '}
+      </Button>
+    </BlueGrid>
   );
 };
 
